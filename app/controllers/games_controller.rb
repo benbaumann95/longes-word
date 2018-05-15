880e9fc1ea3fa38eff934593de1c3bf4291b975d
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-05-15 14:51:44 +0100

    longest word rails

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,44 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
+  def new
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
+  end
+
+  def score
+
+    # response = open("https://wagon-dictionary.herokuapp.com/#{params[:word]})"
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+    @compute_score = compute_score(@word)
+
+
+
+  end
+
+  private
+
+
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+
+  def compute_score(attempt)
+    attempt.size ** 2
+  end
+
+end
